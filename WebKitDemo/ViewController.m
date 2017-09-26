6aa0b3c57501b7baa9ead7161e242d1120cfc457
xiaoanning <734871221@qq.com>
2017-09-26 11:17:52 +0800

    add

--- WebKitDemo/ViewController.m
+++ WebKitDemo/ViewController.m
@@ -7,23 +7,49 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
+
+@property ( nonatomic , strong ) WKWebView * webView ;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    [self createWebView];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 创建webview
+-(void)createWebView
+{
+    WKWebViewConfiguration * config = [[WKWebViewConfiguration alloc]init];
+    [config setProcessPool:[self getProcessPool]];
+    
+    _webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-64) configuration:config];
+    _webView.backgroundColor = [UIColor greenColor];
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:100]];
+    
+    [self.view addSubview:_webView];
+    
 }
+#pragma mark 配置进程池
+//1、WKProcessPool类中没有暴露任何属性和方法，配置为同一个进程池的WebView会共享数据，例如Cookie、用户凭证等，开发者可以通过编写管理类来分配不同维度的WebView在不同进程池中。
+-(WKProcessPool *)getProcessPool
+{
+    WKProcessPool * processPool = [[WKProcessPool alloc]init];
+    return processPool ;
+}
+
+#pragma mark
+
 
 
 @end
